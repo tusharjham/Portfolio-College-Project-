65753ce66a366120d662034fdf599c28ae016a9f
Tushar <jhamtushar12@gmail.com>
2022-08-04 21:53:15 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#typer", {
+  stringsElement: "#typed-strings",
+  smartBackspace: true,
+  typeSpeed: 5,
+  backSpeed: 5,
+  loop: true,
+  loopCount: Infinity,
+  cursorChar: "",
+});
